940694da84747023c783f894e7916444b4199d0a
Kevin S Delay <ksdelay@silk3.uvm.edu>
2015-10-06 18:59:27 -0400

    restart

---
+++ sitemap.php
@@ -0,0 +1,20 @@
+<html lang="en">
+    <head>      <link rel="stylesheet"
+                      href="cssStyle.css"
+                      type="text/css"
+                      media="screen">
+
+        <title>Test</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Kevin Delay">
+        <meta name="t" content="">
+    </head>
+
+    <body>
+    <li><a href="assignment1.0/index.php"> Assignment 1</a></li>
+    <li><a href="assignment2.0/index.php">Assignment 2</a></li>
+        
+
+
+    </body>
+</html>
